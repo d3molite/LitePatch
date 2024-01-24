a3464ed0b13b7dc31ec961f635e90d7e06d1e66a
Severin Kleesattel <severin.kleesattel@cherubim.de>
2024-01-24 13:41:06 +0700

    PR Fixes

--- src/LitePatch/LitePatch.Services/Interfaces/ISettingsService.cs
+++ src/LitePatch/LitePatch.Services/Interfaces/ISettingsService.cs
@@ -5,9 +5,6 @@ namespace LitePatch.Services.Interfaces;
 
 public interface ISettingsService
 {
-    public SettingsRepository SettingsRepository { get; set; }
     public ApplicationSetting Settings { get; set; }
-
-
     public void Save();
 }
