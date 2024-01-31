1f18f8926f78764ac0492b730ab09a4a4c818179
Severin Kleesattel <severin.klee@gmail.com>
2024-01-31 14:34:50 +0700

    Issue-4 PatchInfo

---
+++ src/LitePatch/LitePatch.Services/Interfaces/IPatchInfo.cs
@@ -0,0 +1,11 @@
+﻿using LibGit2Sharp;
+using LitePatch.Services.Models;
+
+namespace LitePatch.Services.Interfaces;
+
+public interface IPatchInfo
+{
+    string GetPatchFilePath();
+    void MarkAsApplied();
+
+}
