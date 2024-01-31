bc48d44a3c156d4d13d1f8d581f05eab0181ce78
Severin Kleesattel <severin.klee@gmail.com>
2024-01-31 14:34:50 +0700

    Issue-4 PatchInfo

---
+++ src/LitePatch/LitePatch.Services/Models/PatchInfo.cs
@@ -0,0 +1,38 @@
+﻿using LitePatch.Services.Interfaces;
+
+namespace LitePatch.Services.Models;
+
+public class PatchInfo : IPatchInfo
+{
+    private string _patchName { get; }
+    private string _patchPath { get; }
+    private bool _hasbeenApplied;
+
+    public PatchInfo(string commitName, string patchPath)
+    {
+        _patchName = commitName;
+        _patchPath = patchPath;
+        _hasbeenApplied = false;
+
+    }
+
+    public string GetPatchName()
+    {
+        return _patchName;
+    }
+    
+    public string GetPatchFilePath()
+    {
+        return _patchPath;
+    }
+
+    public bool AppliedStatus()
+    {
+        return _hasbeenApplied;
+    }
+
+    public void MarkAsApplied()
+    {
+        _hasbeenApplied = true;
+    }
+}
