0450305a2ff3733325e878b73f823b754682c73b
Severin Kleesattel <severin.kleesattel@cherubim.de>
2024-02-01 15:43:18 +0700

    [PR-Fixes] Whitespace & Usings fixed

--- src/LitePatch/LitePatch.Main/MainWindow.xaml.cs
+++ src/LitePatch/LitePatch.Main/MainWindow.xaml.cs
@@ -3,7 +3,6 @@ using LitePatch.Services.Interfaces;
 using LitePatch.Services.Repo;
 using LitePatch.Services.Services;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection.Extensions;
 using MudBlazor.Services;
 
 namespace LitePatch;
--- src/LitePatch/LitePatch.Services/Interfaces/IGitPatchService.cs
+++ src/LitePatch/LitePatch.Services/Interfaces/IGitPatchService.cs
@@ -5,11 +5,10 @@ namespace LitePatch.Services.Interfaces;
 public interface IGitPatchService
 {
     public List<PatchInfo> PatchList { get; set; }
-    public string RepoFolderPath { get; set; }
-    public void ExportPatch(string sha, string commitName);
-    public void ApplyPatch(PatchInfo patch);
-
     
-
-
+    public string RepoFolderPath { get; set; }
+    
+    public void ExportPatch(string sha, string commitName);
+    
+    public void ApplyPatch(PatchInfo patch);
 }
